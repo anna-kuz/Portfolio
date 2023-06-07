8be45d7f7959ce98eca1025a4cd619f46220caf5
annakuz <youremail@domain.com>
2023-06-07 13:38:39 -0700

    finished abt me page, just need to style it

--- About-Me.js
+++ About-Me.js
@@ -1,3 +1,12 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+
 const images = [
     "IMG_E6117.JPG",
     "IMG_E6111.JPG",
@@ -54,3 +63,22 @@ document.addEventListener("touchend", function (event) {
     event.preventDefault
     placeImage(event.pageX, event.pageY)
 })
+
+
+
+
+
+// // <header class="header">
+// {/* <div class="name-logo">
+//     <a href="Home-Page.html" rel="norefferer noopener"><span class="first">𝐚𝐧𝐧𝐚</span><span class="last">𝐤𝐮𝐳𝐦𝐢𝐧</a>
+//   </div> */}
+
+// // <nav class="navbar">
+// // <a class="links">
+// //     <a href="About-Me.html" rel="norefferer noopener">About Me</a>
+// //     <a href="Resume.html" rel="norefferer noopener">Resume</a>
+// //     <a href="Projects.html" rel="norefferer noopener">Projects</a>
+// // </a>
+// // </div>
+// // </nav>
+// // </header>

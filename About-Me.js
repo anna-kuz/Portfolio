7136ce008c22a28a528f8bce4054cbdab284b053
annakuz <youremail@domain.com>
2023-06-08 13:00:26 -0700

    starting proj page scrolling bg

--- About-Me.js
+++ About-Me.js
@@ -7,78 +7,60 @@ menu.onclick = () => {
 }
 
 
-const images = [
-    "IMG_E6117.JPG",
-    "IMG_E6111.JPG",
-    "IMG_E6113.JPG",
-    "IMG_E6114.JPG",
-    "IMG_E6115.JPG",
-    "IMG_E6116.JPG",
-    "IMG_E6110.JPG"
-]
+// const images = [
+//     "IMG_E6117.JPG",
+//     "IMG_E6111.JPG",
+//     "IMG_E6113.JPG",
+//     "IMG_E6114.JPG",
+//     "IMG_E6115.JPG",
+//     "IMG_E6116.JPG",
+//     "IMG_E6110.JPG"
+// ]
 
-const size = [
-    "280px",
-    "150px",
-    "200px",
-    "300px",
-    "200px",
-    "300px",
-    "250px"
-]
+// const size = [
+//     "280px",
+//     "150px",
+//     "200px",
+//     "300px",
+//     "200px",
+//     "300px",
+//     "250px"
+// ]
 
 
-let i = 0
+// let i = 0
 
-function placeImage(x, y) {
+// function placeImage(x, y) {
 
-    const nextImage = images[i]
+//     const nextImage = images[i]
 
-    const img = document.createElement("img")
-    img.setAttribute("src", nextImage)
-    img.style.width = size[i]
-    img.style.left = x + "px"
-    img.style.top = y + "px"
+//     const img = document.createElement("img")
+//     img.setAttribute("src", nextImage)
+//     img.style.width = size[i]
+//     img.style.left = x + "px"
+//     img.style.top = y + "px"
     
 
 
-    document.body.appendChild(img)
+//     document.body.appendChild(img)
 
-    i = i + 1
+//     i = i + 1
 
-    if (i>= images.length) {
-        i = 0
-    } 
-}
-
-
-
-document.addEventListener("click", function (event) {
-    event.preventDefault()
-    placeImage(event.pageX, event.pageY)
-})
-
-
-document.addEventListener("touchend", function (event) {
-    event.preventDefault
-    placeImage(event.pageX, event.pageY)
-})
+//     if (i>= images.length) {
+//         i = 0
+//     } 
+// }
 
 
 
+// document.addEventListener("click", function (event) {
+//     event.preventDefault()
+//     placeImage(event.pageX, event.pageY)
+// })
 
 
-// // <header class="header">
-// {/* <div class="name-logo">
-//     <a href="Home-Page.html" rel="norefferer noopener"><span class="first">𝐚𝐧𝐧𝐚</span><span class="last">𝐤𝐮𝐳𝐦𝐢𝐧</a>
-//   </div> */}
+// document.addEventListener("touchend", function (event) {
+//     event.preventDefault
+//     placeImage(event.pageX, event.pageY)
+// })
 
-// // <nav class="navbar">
-// // <a class="links">
-// //     <a href="About-Me.html" rel="norefferer noopener">About Me</a>
-// //     <a href="Resume.html" rel="norefferer noopener">Resume</a>
-// //     <a href="Projects.html" rel="norefferer noopener">Projects</a>
-// // </a>
-// // </div>
-// // </nav>
-// // </header>

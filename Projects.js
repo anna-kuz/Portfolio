94f5c9afb6dbedb770658f60b1027f2b7a4eacca
annakuz <youremail@domain.com>
2023-06-10 22:40:42 -0700

    changing css stuff

--- Projects.js
+++ Projects.js
@@ -7,6 +7,37 @@ menu.onclick = () => {
 }
 
 
+// window.addEventListener('scroll', () => {
+//     let content = document.querySelector('.card');
+//     let contentPosition = content.getBoundingClientRect().top;
+//     let screenPosition = window.innerHeight;
+//     if (contentPosition < screenPosition) {
+//        content.classList.add('.active')
+//     }
+// });
+
+
+
+// const cards = document.querySelectorAll('.card');
+
+// window.addEventListener('scroll', checkCards);
+
+// checkCards();
+
+// function checkCards() {
+//     const triggerBottom = window.innerHeight / 5 * 4;
+
+//     cards.forEach(card => {
+//         const cardTop = card.getBoundingClientRect().top;
+//         if (cardTop < triggerBottom) {
+//             card.classList.add('show');
+//         } else {
+//             box.classList.remove('show');
+//         }
+//     });
+// }
+
+
 
 
 

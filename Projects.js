65ce83d383b0a6a0a2b7c49a61dbdc8e037abd4c
annakuz <youremail@domain.com>
2023-06-12 09:05:56 -0700

    submitting

--- Projects.js
+++ Projects.js
@@ -44,23 +44,23 @@ icon.onclick = function() {
     }
 }
 
-const faders = document.querySelectorAll('.fade-in');
-const appearOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px -100px 0px"
-};
+// const faders = document.querySelectorAll('.fade-in');
+// const appearOptions = {
+//     threshold: 1,
+//     rootMargin: "0px 0px -100px 0px"
+// };
 
-const appearOnScroll = new IntersectionObserver (function(entries, appearOnScroll) {
-    entries.forEach(entry => {
-        if(!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target);
-        }
-    });
-}, 
-appearOptions);
+// const appearOnScroll = new IntersectionObserver (function(entries, appearOnScroll) {
+//     entries.forEach(entry => {
+//         if(!entry.isIntersecting) {
+//             return;
+//         } else {
+//             entry.target.classList.add('appear');
+//             appearOnScroll.unobserve(entry.target);
+//         }
+//     });
+// }, 
+// appearOptions);
 
 
 // faders.forEach(fader => {
@@ -77,35 +77,35 @@ appearOptions);
 
 
 
-// window.addEventListener('scroll', () => {
-//     let content = document.querySelector('.card');
-//     let contentPosition = content.getBoundingClientRect().top;
-//     let screenPosition = window.innerHeight;
-//     if (contentPosition < screenPosition) {
-//        content.classList.add('.active')
-//     }
-// });
+window.addEventListener('scroll', () => {
+    let content = document.querySelector('.card');
+    let contentPosition = content.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if (contentPosition < screenPosition) {
+       content.classList.add('.active')
+    }
+});
 
 
 
-// const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card');
 
-// window.addEventListener('scroll', checkCards);
+window.addEventListener('scroll', checkCards);
 
-// checkCards();
+checkCards();
 
-// function checkCards() {
-//     const triggerBottom = window.innerHeight / 5 * 4;
+function checkCards() {
+    const triggerBottom = window.innerHeight / 5 * 4;
 
-//     cards.forEach(card => {
-//         const cardTop = card.getBoundingClientRect().top;
-//         if (cardTop < triggerBottom) {
-//             card.classList.add('show');
-//         } else {
-//             box.classList.remove('show');
-//         }
-//     });
-// }
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+        if (cardTop < triggerBottom) {
+            card.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    });
+}
 
 
 

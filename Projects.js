ffe69b32c09d4e88b60f72098281f730279e2cd9
annakuz <youremail@domain.com>
2023-06-11 20:18:50 -0700

    broke and restarted proj page

--- Projects.js
+++ Projects.js
@@ -7,6 +7,49 @@ menu.onclick = () => {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+// const faders = document.querySelectorAll('.fade-in');
+// const appearOptions = {
+//     threshold: 1,
+//     rootMargin: "0px 0px -100px 0px"
+// };
+
+// const appearOnScroll = new IntersectionObserver (function(entries, appearOnScroll) {
+//     entries.forEach(entry => {
+//         if(!entry.isIntersecting) {
+//             return;
+//         } else {
+//             entry.target.classList.add('appear');
+//             appearOnScroll.unobserve(entry.target);
+//         }
+//     });
+// }, 
+// appearOptions);
+
+
+// faders.forEach(fader => {
+//     appearOnScroll.observe(fader);
+// })
+
+
+
+
+
+
+
+
+
+
+
 // window.addEventListener('scroll', () => {
 //     let content = document.querySelector('.card');
 //     let contentPosition = content.getBoundingClientRect().top;
